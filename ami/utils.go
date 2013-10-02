85c08173b657de984493851a0c51efe6487d7958
Helton Marques <helton.marx@gmail.com>
2013-10-02 18:43:06 -0300

    create a single function to get message lists:w

--- ami/utils.go
+++ ami/utils.go
@@ -2,6 +2,7 @@ package ami
 
 import (
 	"strings"
+    "errors"
 )
 
 func decode(socket *Socket) (map[string]string, error) {
@@ -27,3 +28,61 @@ func decode(socket *Socket) (map[string]string, error) {
 on_exit:
 	return message, nil
 }
+
+const (
+	getResponseState int = iota
+	getListState
+)
+
+func getMessageList(socket *Socket, action, actionID, event, complete string) ([]map[string]string, error) {
+	// verify socket
+	if !socket.Connected() {
+		return nil, errors.New("Invalid socket")
+	}
+
+	// verify parameters
+	if len(actionID) == 0 || len(action) == 0 ||
+		len(event) == 0 || len(complete) == 0 {
+		return nil, errors.New("Invalid parameters")
+	}
+
+	command := []string{
+		"Action: ",
+		action,
+		"\r\nActionID: ",
+		actionID,
+		"\r\n\r\n", // end of command
+	}
+
+	err := sendCmd(socket, command)
+	if err != nil {
+		return nil, err
+	}
+
+	list := make([]map[string]string, 0)
+	state := getResponseState
+	for {
+		message, err := decode(socket)
+		if (err != nil) || (message["ActionID"] != actionID) {
+			return nil, err
+		}
+		switch state {
+		case getResponseState:
+			if message["Response"] != "Success" {
+				return nil, errors.New(message["Message"])
+			} else {
+				state = getListState
+			}
+		case getListState:
+			if message["Event"] == complete {
+				goto on_exit
+			} else if message["Event"] == event {
+				list = append(list, message)
+			}
+		}
+	}
+on_exit:
+	return list, nil
+
+}
+
