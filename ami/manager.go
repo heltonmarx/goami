36568e34b5569e01de301f3a8fcf2a013e2fc412
Helton Marques <helton.marx@gmail.com>
2013-12-27 12:10:15 -0200

    implements some manager commands

--- ami/manager.go
+++ ami/manager.go
@@ -125,3 +125,175 @@ func Ping(socket *Socket, actionID string) (bool, error) {
 	}
 	return true, nil
 }
+
+//	Challenge
+//		Generate a challenge for MD5 authentication.
+//
+func Challenge(socket *Socket, actionID string) (map[string]string, error) {
+	if len(actionID) == 0 {
+		return nil, errors.New("Invalid parameters")
+	}
+	command := []string{
+		"Action: Challenge",
+		"\r\nActionID: ",
+		actionID,
+		"\r\nAuthType: ",
+		"MD5",
+		"\r\n\r\n", // end of command
+	}
+	return getMessage(socket, command, actionID)
+}
+
+//	Command
+//		Execute Asterisk CLI Command.
+//
+func Command(socket *Socket, actionID, cmd string) (map[string]string, error) {
+	if len(actionID) == 0 || len(cmd) == 0 {
+		return nil, errors.New("Invalid parameters")
+	}
+	command := []string{
+		"Action: Command",
+		"\r\nActionID: ",
+		actionID,
+		"\r\nCommand: ",
+		cmd,
+		"\r\n\r\n", // end of command
+	}
+	return getMessage(socket, command, actionID)
+}
+
+//	CoreSettings
+//		Show PBX core settings (version etc).
+//
+func CoreSettings(socket *Socket, actionID string) (map[string]string, error) {
+	if len(actionID) == 0 {
+		return nil, errors.New("Invalid parameters")
+	}
+	command := []string{
+		"Action: CoreSettings",
+		"\r\nActionID: ",
+		actionID,
+		"\r\n\r\n", // end of command
+	}
+	return getMessage(socket, command, actionID)
+}
+
+//	CoreStatus
+//		Show PBX core status variables.
+//
+func CoreStatus(socket *Socket, actionID string) (map[string]string, error) {
+	if len(actionID) == 0 {
+		return nil, errors.New("Invalid parameters")
+	}
+	command := []string{
+		"Action: CoreStatus",
+		"\r\nActionID: ",
+		actionID,
+		"\r\n\r\n", // end of command
+	}
+	return getMessage(socket, command, actionID)
+}
+
+//	CreateConfig
+//		Creates an empty file in the configuration directory.
+//		This action will create an empty file in the configuration directory. 
+//		This action is intended to be used before an UpdateConfig action.
+//
+func CreateConfig(socket *Socket, actionID, filename string) (map[string]string, error) {
+	if len(actionID) == 0 || len(filename) == 0 {
+		return nil, errors.New("Invalid parameters")
+	}
+	command := []string{
+		"Action: CreateConfig",
+		"\r\nActionID: ",
+		actionID,
+		"\r\nFilename: ",
+		filename,
+		"\r\n\r\n", // end of command
+	}
+	return getMessage(socket, command, actionID)
+}
+
+//	DataGet
+//		Retrieve the data api tree.
+//
+func DataGet(socket *Socket, actionID, path, search, filter string) (map[string]string, error) {
+	if len(actionID) == 0 || len(path) == 0 ||
+		len(search) == 0 || len(filter) == 0 {
+		return nil, errors.New("Invalid parameters")
+	}
+	command := []string{
+		"Action: DataGet",
+		"\r\nActionID: ",
+		actionID,
+		"\r\nPath: ",
+		path,
+		"\r\nSearch: ",
+		search,
+		"\r\nFilter: ",
+		filter,
+		"\r\n\r\n", // end of command
+	}
+	return getMessage(socket, command, actionID)
+}
+
+//	Events
+//		Control Event Flow.
+//		Enable/Disable sending of events to this manager client.
+//
+func Events(socket *Socket, actionID, eventMask string) (map[string]string, error) {
+	if len(actionID) == 0 || len(eventMask) == 0 {
+		return nil, errors.New("Invalid parameters")
+	}
+	command := []string{
+		"Action: Events",
+		"\r\nActionID: ",
+		actionID,
+		"\r\nEventMask: ",
+		eventMask,
+		"\r\n\r\n", // end of command
+	}
+	return getMessage(socket, command, actionID)
+}
+
+//	GetConfig
+//		Retrieve configuration.
+//		This action will dump the contents of a configuration file by category and contents or optionally by specified category only.
+//
+func GetConfig(socket *Socket, actionID, filename, category string) (map[string]string, error) {
+	if len(actionID) == 0 || len(filename) == 0 || len(category) == 0 {
+		return nil, errors.New("Invalid parameters")
+	}
+	command := []string{
+		"Action: GetConfig",
+		"\r\nActionID: ",
+		actionID,
+		"\r\nFilename: ",
+		filename,
+		"\r\nCategory: ",
+		category,
+		"\r\n\r\n", // end of command
+	}
+	return getMessage(socket, command, actionID)
+}
+
+//	GetConfigJSON
+//		Retrieve configuration (JSON format).
+//		This action will dump the contents of a configuration file by category and contents in JSON format. 
+//		This only makes sense to be used using rawman over the HTTP interface.
+//
+func GetConfigJSON(socket *Socket, actionID, filename string) (map[string]string, error) {
+	if len(actionID) == 0 || len(filename) == 0 {
+		return nil, errors.New("Invalid parameters")
+	}
+	command := []string{
+		"Action: GetConfigJSON",
+		"\r\nActionID: ",
+		actionID,
+		"\r\nFilename: ",
+		filename,
+		"\r\n\r\n", // end of command
+	}
+	return getMessage(socket, command, actionID)
+}
+
