13fa30bbc4e385f57104136441a4ddee2a2ab46a
iglov <iglov@avalon.land>
2025-04-07 10:48:56 +0200

    fix: always return any kind of errors (#45)

--- ami/socket.go
+++ ami/socket.go
@@ -84,9 +84,6 @@ func (s *Socket) run(ctx context.Context, conn net.Conn) {
 	for {
 		msg, err := reader.ReadString('\n')
 		if err != nil {
-			if err == io.EOF && len(msg) == 0 {
-				return
-			}
 			s.errors <- err
 			return
 		}
